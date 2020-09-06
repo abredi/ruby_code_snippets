f52f21b074894bd694d87e04f677f80c4b667658
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-09-06 17:44:11 +0300

    Add Selenium google search feature

---
+++ .history/bin/console_20200906173832.rb
@@ -0,0 +1,3 @@
+require "../lib/selenium"
+
+AASS.selenium
---
+++ .history/lib/selenium_20200906174152.rb
@@ -0,0 +1,22 @@
+require "selenium-webdriver"
+
+module AASS
+  class Error < StandardError; end
+
+  def self.selenium
+    driver = Selenium::WebDriver.for :chrome
+    driver.navigate.to "http://google.com"
+
+    element = driver.find_element(name: 'q')
+    element.send_keys "software automated testing"
+    element.submit
+
+    res = driver.find_element(:css, '.g a')
+
+    res.click
+
+    puts driver.title
+
+    driver.quit
+  end
+end
